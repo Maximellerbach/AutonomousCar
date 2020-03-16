9b8f2426c59c27ff157d5a7358cfc13d4d9765b5
Maxime Ellerbach <maxime@ellerbach.net>
2020-03-16 16:54:43 +0100

    added features to track estimation

--- create_model/convolution/track_estimation.py
+++ create_model/convolution/track_estimation.py
@@ -1,12 +1,15 @@
+import autolib
 import math
 from glob import glob
 
 import cv2
 import numpy as np
+from scipy.spatial.distance import euclidean
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
-import autolib
 import reorder_dataset
+
 
 class pos_map():
     def __init__(self, size=(512, 512), its=30, steer_coef=30):
@@ -17,18 +20,31 @@ class pos_map():
 
         self.angle = 0
 
+    def normalize_pt(self, p, maxp, minp, entier=True, shape=(512, 512)):
+        h, w = shape
+
+        x = ((p[0]-minp)/(maxp-minp))*w
+        y = ((p[1]-minp)/(maxp-minp))*h
+        if entier == True:
+            x = int(x)
+            y = int(y)
+
+        return (x, y)
+
     def get_pos(self, paths, Y, speed=1):
-        degs = []
+        vect_list = []
         pos_list = []
+        minpos_list = []
+        degs = []
         pos = [0, 0]
         angle = 0
 
-        for p, ny in zip(paths, Y):
+        for it, ny in enumerate(Y):
             average = 0
             coef = [1, 0.58, 0, -0.58, -1]
 
-            for it, nyx in enumerate(ny):
-                average += nyx*coef[it]
+            for ait, nyx in enumerate(ny):
+                average += nyx*coef[ait]
 
             degree_angle = average*self.steer_coef*self.dt
             rad = math.radians(degree_angle)
@@ -42,13 +58,84 @@ class pos_map():
             pos[1] += y
 
             # print(x, y, dt) # debuging
-
             pos_list.append((pos[0], pos[1]))
-            degs.append(degree_angle)
+            vect_list.append((x, y))
+            degs.append(rad)
 
-        return pos_list, degs
+            if degree_angle!=0. or it==0:
+                minpos_list.append((pos[0], pos[1], it))
+            
 
-    def draw(self, paths, pos_list, degs):
+        return pos_list, minpos_list, vect_list, degs
+
+
+    def boundaries(self, pos_list):
+        iner = []
+        outer = []
+
+        offset = 1
+        for p1, p2 in zip(pos_list[:-1], pos_list[1:]):
+            x1, y1 = p1[:2]
+            x2, y2 = p2[:2]
+
+            way = 1 if x2-x1>0 else -1
+
+            if (x2-x1) != 0:
+                a = (y2-y1)/(x2-x1)
+
+                angle = math.pi/2+math.atan(a)
+                wi = math.cos(angle)*offset*way
+                hi = math.sin(angle)*offset*way
+
+                iner.append((x1+wi,y1+hi))
+                outer.append((x1-wi,y1-hi))
+
+            else:
+                iner.append((x1+offset*way, y1))
+                outer.append((x1-offset*way, y1))
+
+        return iner, outer
+
+
+    def segment_track(self, pos_list, deg_list, th=0.005, look_back=30):
+        turning = False
+        way = 0
+        average = [0]*len(deg_list)
+        thresholded = []
+        turn = []
+
+        for it in range(len(deg_list)):
+            if it>=look_back:
+                average[it] = np.average(deg_list[it-look_back:it])
+
+                if average[it]>=th:
+                    if turning == False:
+                        way = 1
+                        x, y = pos_list[it]
+                        turn.append((x,y,it,way))
+                        turning = True
+
+                elif average[it]<=-th:
+                    if turning == False:
+                        way = -1
+                        x, y = pos_list[it]
+                        turn.append((x,y,it,way))
+                        turning = True
+
+                elif turning == True:
+                    x, y = pos_list[it]
+                    turn.append((x,y,it,way))
+                    thresholded.append(turn)
+                    turning = False
+                    turn = []
+                    way = 0
+
+        return thresholded, average
+
+    def count_segments(self, pos_list):
+        return
+        
+    def draw(self, pos_list, degs=[]):
         maxp = np.max(pos_list)
         minp = np.min(pos_list)
 
@@ -56,46 +143,45 @@ class pos_map():
         for it, p in enumerate(pos_list):
             rpx = ((p[0]-minp)/(maxp-minp))*w
             rpy = ((p[1]-minp)/(maxp-minp))*h
-            if it==0:
-                color = (0, 0, 1)
-                th = 3
-            elif degs[it]>0:
-                color = (1, 0, 0)
-                th = 1
-            elif degs[it]<0:
-                color = (0, 0, 1)
-                th = 1
-            else:
+            try:
+                if it==0:
+                    color = (0, 0, 1)
+                    th = 3
+                elif degs[it]>0:
+                    color = (1, 0, 0)
+                    th = 1
+                elif degs[it]<0:
+                    color = (0, 0, 1)
+                    th = 1
+                else:
+                    color = (0, 1, 0)
+            except:
                 color = (0, 1, 0)
                 th = 1
 
+
             cv2.circle(self.pmap, (int(rpx), int(rpy)), 1, color, thickness=th)
-            img = cv2.imread(paths[it])
-            
-            cv2.imshow('img', img)
-            cv2.imshow('pmap', self.pmap)
-            cv2.waitKey(1)
-
-        cv2.imshow('pmap', self.pmap)
-        cv2.waitKey(0)
     
-    def boundaries(self, pos_list):
-        iner = []
-        outer = []
 
-        offset = 10
+    def draw_segments(self, segments):
+        segments = np.array(segments)
+        segments_points = segments[:, :, :2]
+        maxp = np.max(segments_points)
+        minp = np.min(segments_points)
 
-        for p in pos_list:
-            x, y = p
-            iner.append((x, y))
+        for segm in segments:
+            p1 = self.normalize_pt(segm[0], maxp, minp)
+            p2 = self.normalize_pt(segm[1], maxp, minp)
+            way = segm[0][-1]
 
+            if way>0:
+                color = (1, 0, 0)
+            elif way<0:
+                color = (0, 0, 1)
 
-        return
-        
+            cv2.line(self.pmap, p1, p2, color, thickness=1)
 
 if __name__ == "__main__":
-    # X = load_dataset()
-    # Y = get_preds(X)
     dts, datalen = reorder_dataset.load_dataset('C:\\Users\\maxim\\datasets\\1\\', recursive=False)
     
     ts = date = reorder_dataset.get_date(dts[0])
@@ -104,7 +190,6 @@ if __name__ == "__main__":
 
     datalen = len(dts)
     its = datalen/dt
-
 
     Y = []
     for d in dts:
@@ -115,6 +200,24 @@ if __name__ == "__main__":
     Y = autolib.label_smoothing(Y, 5, 0) # to categorical
 
     pmap = pos_map(its=its, steer_coef=35)
-    pos_list, degs = pmap.get_pos(dts, Y)
-    pmap.draw(dts, pos_list, degs)
-    # pmap.boundaries()
+    pos_list, minpos_list, vect_list, deg_list = pmap.get_pos(dts, Y[:])
+
+    turns_segments, average = pmap.segment_track(pos_list, deg_list, look_back=60)
+    plt.plot([i for i in range(len(vect_list))], np.array(vect_list)[:, 1], np.array(vect_list)[:, 0], linewidth=1)
+    plt.plot([i for i in range(len(average))], average, linewidth=1)
+    plt.show()
+
+
+    pmap.draw_segments(turns_segments)
+    cv2.imshow('pmap', pmap.pmap)
+    cv2.waitKey(0)
+    pmap.pmap = np.zeros_like(pmap.pmap)
+
+
+    iner, outer = pmap.boundaries(pos_list)
+    diner = [1 for i in range(len(iner))]
+    douter = [-1 for i in range(len(outer))]
+    pmap.draw(pos_list+iner+outer, degs=deg_list+diner+douter)
+    cv2.imshow('pmap', pmap.pmap)
+    cv2.waitKey(0)
+
