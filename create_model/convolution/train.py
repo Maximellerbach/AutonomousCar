07b67b82054601ba1295ef21a09287d433049993
Maxime Ellerbach <maxim@Laurent-PC.(none)>
2020-04-13 22:20:19 +0200

    model update

--- create_model/convolution/train.py
+++ create_model/convolution/train.py
@@ -72,7 +72,7 @@ class classifier():
         
         else:
             # model, fe = model_type((120, 160, 3), 5, loss="mse", prev_act="relu", last_act="relu", regularizer=(0, 0), last_bias=True, recurrence=self.recurrence, memory=self.memory_size, metrics=["binary_crossentropy", "mse"])
-            model, fe = model_type((120, 160, 3), 5, loss="categorical_crossentropy", prev_act="relu", last_act="softmax", regularizer=(0.05, 0.05), recurrence=self.recurrence, memory=self.memory_size)
+            model, fe = model_type((120, 160, 3), 5, loss="categorical_crossentropy", prev_act="relu", last_act="softmax", regularizer=(0, 0), recurrence=self.recurrence, memory=self.memory_size, metrics=["categorical_accuracy", "mse"])
 
             
             # model, fe = architectures.create_DepthwiseConv2D_CNN((120, 160, 3), 5)
@@ -279,14 +279,14 @@ class classifier():
 
 if __name__ == "__main__":
     AI = classifier(name = 'test_model\\convolution\\lightv6_mix.h5', dospath ='C:\\Users\\maxim\\datasets\\*',
-                    recurrence=False, dosdir=True, proportion=0.2, to_cat=True, smoothing=0.2, label_rdm=0.0) 
+                    recurrence=False, dosdir=True, proportion=0.4, to_cat=True, smoothing=0.2, label_rdm=0.0) 
                     # name of the model, path to dir dataset, set reccurence for data loading, set dosdir for data loading, set proportion of upscaled/function
 
     AI.epochs = 2
-    AI.batch_size = 256 # without augm; normally, high batch_size = better comprehension
+    AI.batch_size = 128 # without augm; normally, high batch_size = better comprehension
 
     AI.train(load=False)
-    AI.model = load_model(AI.name, custom_objects={"dir_loss":architectures.dir_loss})
+    AI.model = load_model(AI.name) # custom_objects={"dir_loss":architectures.dir_loss}
     # print(AI.calculate_FLOPS(), "total ops")
     # print(AI.evaluate_speed())
 
