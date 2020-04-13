168ac205eb274649de08ea23952377cf64168fb8
Maxime Ellerbach <maxim@Laurent-PC.(none)>
2020-04-13 22:45:07 +0200

    model

--- create_model/convolution/train.py
+++ create_model/convolution/train.py
@@ -71,8 +71,8 @@ class classifier():
             fe = load_model('test_model\\convolution\\fe.h5')
         
         else:
-            # model, fe = model_type((120, 160, 3), 5, loss="mse", prev_act="relu", last_act="relu", regularizer=(0, 0), last_bias=True, recurrence=self.recurrence, memory=self.memory_size, metrics=["binary_crossentropy", "mse"])
-            model, fe = model_type((120, 160, 3), 5, loss="categorical_crossentropy", prev_act="relu", last_act="softmax", regularizer=(0, 0), recurrence=self.recurrence, memory=self.memory_size, metrics=["categorical_accuracy", "mse"])
+            model, fe = model_type((120, 160, 3), 5, loss="mae", prev_act="relu", last_act="relu", regularizer=(0, 0), last_bias=True, recurrence=self.recurrence, memory=self.memory_size, metrics=["mae", "mse"])
+            # model, fe = model_type((120, 160, 3), 5, loss="categorical_crossentropy", prev_act="relu", last_act="softmax", regularizer=(0, 0), recurrence=self.recurrence, memory=self.memory_size, metrics=["categorical_accuracy", "mse"])
 
             
             # model, fe = architectures.create_DepthwiseConv2D_CNN((120, 160, 3), 5)
@@ -279,10 +279,10 @@ class classifier():
 
 if __name__ == "__main__":
     AI = classifier(name = 'test_model\\convolution\\lightv6_mix.h5', dospath ='C:\\Users\\maxim\\datasets\\*',
-                    recurrence=False, dosdir=True, proportion=0.4, to_cat=True, smoothing=0.2, label_rdm=0.05) 
+                    recurrence=False, dosdir=True, proportion=0.4, to_cat=True, smoothing=0.2, label_rdm=0.0) 
                     # name of the model, path to dir dataset, set reccurence for data loading, set dosdir for data loading, set proportion of upscaled/function
 
-    AI.epochs = 3
+    AI.epochs = 1
     AI.batch_size = 64 # without augm; normally, high batch_size = better comprehension
 
     AI.train(load=False)
