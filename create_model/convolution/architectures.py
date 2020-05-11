d231c04d5f8d8d0a0913f50fe5b4bc10aa7c6eb7
Maxime Ellerbach <maxim@Laurent-PC.(none)>
2020-05-12 00:31:58 +0200

    new model

--- create_model/convolution/architectures.py
+++ create_model/convolution/architectures.py
@@ -40,22 +40,18 @@ def create_light_CNN(img_shape, number_class, prev_act="relu", last_act="softmax
     # x = GaussianNoise(0.2)(inp)
     
     x = Conv2D(12, kernel_size=5, strides=2, use_bias=False, padding='same')(inp)
-    x = BatchNormalization()(x)
     x = Activation(prev_act)(x)
     x = Dropout(drop_rate)(x)
 
     x = Conv2D(16, kernel_size=5, strides=2, use_bias=False, padding='same')(x)
-    x = BatchNormalization()(x)
     x = Activation(prev_act)(x)
     x = Dropout(drop_rate)(x)
 
     x = Conv2D(32, kernel_size=3, strides=2, use_bias=False, padding='same')(x)
-    x = BatchNormalization()(x)
     x = Activation(prev_act)(x)
     x = Dropout(drop_rate)(x)
     
     x = Conv2D(48, kernel_size=3, strides=2, use_bias=False, padding='same')(x)
-    x = BatchNormalization()(x)
     x = Activation(prev_act)(x)
     x = Dropout(drop_rate)(x)
 
