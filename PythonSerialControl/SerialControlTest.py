44048e3c46e3a0a6b34dd571db36f155ba0a1396
Maxime Ellerbach <maxim@Laurent-PC.(none)>
2020-05-18 20:05:07 +0200

    quick update

--- PythonSerialControl/SerialControlTest.py
+++ PythonSerialControl/SerialControlTest.py
@@ -37,39 +37,39 @@ for opt, arg in opts:
         try:
             print("Turns:" + str(ser.GetTurns()))
 
-            # print("start changing PWM from 0 to 255")
-            # for pwm in range(0,255):
-            #     ser.ChangePWM(pwm)
-            #     time.sleep(0.2)
-            #     print("Turns:" + str(ser.GetTurns()))
-            # print("finished changing PWM")
-
-            # print("Turns:" + str(ser.GetTurns()))
-
-            # for dir in SerialCommand.direction:
-            #     ser.ChangeDirection(dir)
-            #     print("changed direction " + str(dir))
-            #     time.sleep(5)
-            #     print("Turns:" + str(ser.GetTurns()))
-
-            # for motor in SerialCommand.motor:
-            #     ser.ChangeMotorA(motor)
-            #     print("changed motor A " + str(motor))
-            #     time.sleep(5)
-            #     print("Turns:" + str(ser.GetTurns()))
-
-            # for motor in SerialCommand.motor:
-            #     ser.ChangeMotorB(motor)
-            #     print("changed motor B " + str(motor))
-            #     time.sleep(5)
-            #     print("Turns:" + str(ser.GetTurns()))
-
-            # ser.ChangeAll(SerialCommand.direction.DIR_STRAIGHT, SerialCommand.motor.MOTOR_STOP, SerialCommand.motor.MOTOR_STOP, 255)
-            # print("changed all")
-
-            while(True):
-                print("Turns:" + str(ser.GetTurns()))
+            print("start changing PWM from 0 to 255")
+            for pwm in range(0,255):
+                ser.ChangePWM(pwm)
                 time.sleep(0.2)
+                print("Turns:" + str(ser.GetTurns()))
+            print("finished changing PWM")
+
+            print("Turns:" + str(ser.GetTurns()))
+
+            for dir in SerialCommand.direction:
+                ser.ChangeDirection(dir)
+                print("changed direction " + str(dir))
+                time.sleep(5)
+                print("Turns:" + str(ser.GetTurns()))
+
+            for motor in SerialCommand.motor:
+                ser.ChangeMotorA(motor)
+                print("changed motor A " + str(motor))
+                time.sleep(5)
+                print("Turns:" + str(ser.GetTurns()))
+
+            for motor in SerialCommand.motor:
+                ser.ChangeMotorB(motor)
+                print("changed motor B " + str(motor))
+                time.sleep(5)
+                print("Turns:" + str(ser.GetTurns()))
+
+            ser.ChangeAll(SerialCommand.direction.DIR_STRAIGHT, SerialCommand.motor.MOTOR_STOP, SerialCommand.motor.MOTOR_STOP, 255)
+            print("changed all")
+
+            # while(True):
+            #     print("Turns:" + str(ser.GetTurns()))
+            #     time.sleep(0.2)
         except KeyboardInterrupt:
             ser.__exit__()
 
