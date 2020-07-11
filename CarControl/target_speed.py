bb588f75592c4d5bfb6f6a646215e932d4adbc9f
Maxime Ellerbach <maxim@Laurent-PC.(none)>
2020-07-11 21:57:07 +0200

    quick update

--- CarControl/target_speed.py
+++ CarControl/target_speed.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
         if time_received != last_received:
             pid.update(current_speed, current_time=last_received)
-            new_pwm = pid.output
+            new_pwm = int(pid.output)
             last_received = time_received
 
             ser.ChangePWM(new_pwm)
