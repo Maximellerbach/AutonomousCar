766adb3290440f1f004336c373a667fc6dd2ad33
Maxime Ellerbach <maxime@ellerbach.net>
2020-12-20 18:57:29 +0100

    update

--- joy_control/gather_images2.py
+++ joy_control/gather_images2.py
@@ -1,5 +1,6 @@
 import os
 import xbox
+from custom_modules import serial_command
 
 dos_save = os.getcwd()+os.path.normpath("/recorded/")
 if not os.path.isdir(dos_save):
@@ -10,7 +11,7 @@ th_direction = 0.05  # 5% threshold
 th_throttle = 0.06  # 6% threshold
 
 comPort = "/dev/ttyUSB0"
-#ser = serial_command2.start_serial(comPort)
+ser = serial_command2.start_serial(comPort)
 joy = xbox.Joystick()
 # cap = cv2.VideoCapture(0)
 
@@ -31,7 +32,7 @@ while not joy.Back():
     throttle = joy_throttle - joy_brake if abs(joy_throttle) > abs(th_throttle) else 0
 
     pwm = MAXTHROTTLE * throttle
-    #ser.ChangeAll(steering, pwm, min=[-1, -1], max=[1, 1])
+    ser.ChangeAll(steering, pwm, min=[-1, -1], max=[1, 1])
 
     # if joy_button_a:
     #     _, img = cap.read()
