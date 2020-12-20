669826509243cddcb609590424eac368cdb9af8d
Maxime Ellerbach <maxime@ellerbach.net>
2020-12-20 16:22:34 +0100

    update

--- tests/motor.py
+++ tests/motor.py
@@ -6,18 +6,18 @@ comPort = '/dev/ttyUSB0'
 ser = serial_command2.start_serial(comPort)
 
 if __name__ == '__main__':
-    for i in range(25):
+    for i in range(50):
         ser.ChangePWM(127+i, 0, 255)
-        time.sleep(0.05)
+        time.sleep(0.1)
 
-    for i in range(25, 0, -1):
+    for i in range(50, 0, -1):
         ser.ChangePWM(127+i, 0, 255)
-        time.sleep(0.05)
+        time.sleep(0.1)
 
     for i in range(25):
         ser.ChangePWM(127-i, 0, 255)
-        time.sleep(0.05)
+        time.sleep(0.1)
 
     for i in range(25, 0, -1):
         ser.ChangePWM(127-i, 0, 255)
-        time.sleep(0.05)
+        time.sleep(0.1)
