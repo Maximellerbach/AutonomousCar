ad34f905501f56a4944c82c9ec851e29601629a0
Maxime Ellerbach <maxime@ellerbach.net>
2020-12-20 16:25:23 +0100

    update

--- tests/motor.py
+++ tests/motor.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
         ser.ChangePWM(127+i, 0, 255)
         time.sleep(0.1)
 
-    for i in range(50):
+    for i in range(100):
         ser.ChangePWM(127-i, 0, 255)
         time.sleep(0.1)
 
-    for i in range(50, 0, -1):
+    for i in range(100, 0, -1):
         ser.ChangePWM(127-i, 0, 255)
         time.sleep(0.1)
