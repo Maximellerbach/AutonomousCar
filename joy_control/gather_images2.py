38e90cb256f499799744b5d11296b951160eb10c
Maxime Ellerbach <maxime@ellerbach.net>
2021-08-26 16:20:15 +0200

    sync

--- joy_control/gather_images2.py
+++ joy_control/gather_images2.py
@@ -26,6 +26,7 @@ th_throttle = 0.06  # 6% threshold
 comPort = "/dev/ttyUSB0"
 ser = serial_command2.start_serial(comPort)
 joy = controller.XboxOneJoystick()
+joy.init()
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
