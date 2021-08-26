5b04bf7ad431ba6c9e235e3dc9eb509780da0fa8
Maxime Ellerbach <maxime@ellerbach.net>
2021-08-26 15:59:11 +0200

    sync with pi

--- joy_control/test_controller3.py
+++ joy_control/test_controller3.py
@@ -1,159 +1,186 @@
-# Released by rdb under the Unlicense (unlicense.org)
-# Based on information from:
-# https://www.kernel.org/doc/Documentation/input/joystick-api.txt
+# script based on https://github.com/autorope/donkeycar/blob/dev/donkeycar/parts/controller.py
 
-import os, struct, array
-from fcntl import ioctl
+import os
+import struct
+import array
 
-# Iterate over the joystick devices.
-print('Available devices:')
 
-for fn in os.listdir('/dev/input'):
-    if fn.startswith('js'):
-        print('  /dev/input/%s' % (fn))
+class Joystick(object):
+    '''
+    An interface to a physical joystick
+    '''
 
-# We'll store the states here.
-axis_states = {}
-button_states = {}
+    def __init__(self, dev_fn='/dev/input/js0'):
+        self.axis_states = {}
+        self.button_states = {}
+        self.axis_names = {}
+        self.button_names = {}
+        self.axis_map = []
+        self.button_map = []
+        self.jsdev = None
+        self.dev_fn = dev_fn
 
-# These constants were borrowed from linux/input.h
-axis_names = {
-    0x00 : 'x',
-    0x01 : 'y',
-    0x02 : 'z',
-    0x03 : 'rx',
-    0x04 : 'ry',
-    0x05 : 'rz',
-    0x06 : 'throttle',
-    0x07 : 'rudder',
-    0x08 : 'wheel',
-    0x09 : 'gas',
-    0x0a : 'brake',
-    0x10 : 'hat0x',
-    0x11 : 'hat0y',
-    0x12 : 'hat1x',
-    0x13 : 'hat1y',
-    0x14 : 'hat2x',
-    0x15 : 'hat2y',
-    0x16 : 'hat3x',
-    0x17 : 'hat3y',
-    0x18 : 'pressure',
-    0x19 : 'distance',
-    0x1a : 'tilt_x',
-    0x1b : 'tilt_y',
-    0x1c : 'tool_width',
-    0x20 : 'volume',
-    0x28 : 'misc',
-}
+    def init(self):
+        try:
+            from fcntl import ioctl
+        except ModuleNotFoundError:
+            self.num_axes = 0
+            self.num_buttons = 0
+            print("no support for fnctl module. joystick not enabled.")
+            return False
 
-button_names = {
-    0x120 : 'trigger',
-    0x121 : 'thumb',
-    0x122 : 'thumb2',
-    0x123 : 'top',
-    0x124 : 'top2',
-    0x125 : 'pinkie',
-    0x126 : 'base',
-    0x127 : 'base2',
-    0x128 : 'base3',
-    0x129 : 'base4',
-    0x12a : 'base5',
-    0x12b : 'base6',
-    0x12f : 'dead',
-    0x130 : 'a',
-    0x131 : 'b',
-    0x132 : 'c',
-    0x133 : 'x',
-    0x134 : 'y',
-    0x135 : 'z',
-    0x136 : 'tl',
-    0x137 : 'tr',
-    0x138 : 'tl2',
-    0x139 : 'tr2',
-    0x13a : 'select',
-    0x13b : 'start',
-    0x13c : 'mode',
-    0x13d : 'thumbl',
-    0x13e : 'thumbr',
+        if not os.path.exists(self.dev_fn):
+            print(self.dev_fn, "is missing")
+            return False
 
-    0x220 : 'dpad_up',
-    0x221 : 'dpad_down',
-    0x222 : 'dpad_left',
-    0x223 : 'dpad_right',
+        '''
+        call once to setup connection to device and map buttons
+        '''
+        # Open the joystick device.
+        print('Opening %s...' % self.dev_fn)
+        self.jsdev = open(self.dev_fn, 'rb')
 
-    # XBox 360 controller uses these codes.
-    0x2c0 : 'dpad_left',
-    0x2c1 : 'dpad_right',
-    0x2c2 : 'dpad_up',
-    0x2c3 : 'dpad_down',
-}
+        # Get the device name.
+        buf = array.array('B', [0] * 64)
+        # JSIOCGNAME(len)
+        ioctl(self.jsdev, 0x80006a13 + (0x10000 * len(buf)), buf)
+        self.js_name = buf.tobytes().decode('utf-8')
+        print('Device name: %s' % self.js_name)
 
-axis_map = []
-button_map = []
+        # Get number of axes and buttons.
+        buf = array.array('B', [0])
+        ioctl(self.jsdev, 0x80016a11, buf)  # JSIOCGAXES
+        self.num_axes = buf[0]
 
-# Open the joystick device.
-fn = '/dev/input/js0'
-print('Opening %s...' % fn)
-jsdev = open(fn, 'rb')
+        buf = array.array('B', [0])
+        ioctl(self.jsdev, 0x80016a12, buf)  # JSIOCGBUTTONS
+        self.num_buttons = buf[0]
 
-# Get the device name.
-#buf = bytearray(63)
-buf = array.array('B', [0] * 64)
-ioctl(jsdev, 0x80006a13 + (0x10000 * len(buf)), buf) # JSIOCGNAME(len)
-js_name = buf.tobytes().rstrip(b'\x00').decode('utf-8')
-print('Device name: %s' % js_name)
+        # Get the axis map.
+        buf = array.array('B', [0] * 0x40)
+        ioctl(self.jsdev, 0x80406a32, buf)  # JSIOCGAXMAP
 
-# Get number of axes and buttons.
-buf = array.array('B', [0])
-ioctl(jsdev, 0x80016a11, buf) # JSIOCGAXES
-num_axes = buf[0]
+        for axis in buf[:self.num_axes]:
+            axis_name = self.axis_names.get(axis, 'unknown(0x%02x)' % axis)
+            self.axis_map.append(axis_name)
+            self.axis_states[axis_name] = 0.0
 
-buf = array.array('B', [0])
-ioctl(jsdev, 0x80016a12, buf) # JSIOCGBUTTONS
-num_buttons = buf[0]
+        # Get the button map.
+        buf = array.array('H', [0] * 200)
+        ioctl(self.jsdev, 0x80406a34, buf)  # JSIOCGBTNMAP
 
-# Get the axis map.
-buf = array.array('B', [0] * 0x40)
-ioctl(jsdev, 0x80406a32, buf) # JSIOCGAXMAP
+        for btn in buf[:self.num_buttons]:
+            btn_name = self.button_names.get(btn, 'unknown(0x%03x)' % btn)
+            self.button_map.append(btn_name)
+            self.button_states[btn_name] = 0
+            #print('btn', '0x%03x' % btn, 'name', btn_name)
 
-for axis in buf[:num_axes]:
-    axis_name = axis_names.get(axis, 'unknown(0x%02x)' % axis)
-    axis_map.append(axis_name)
-    axis_states[axis_name] = 0.0
+        return True
 
-# Get the button map.
-buf = array.array('H', [0] * 200)
-ioctl(jsdev, 0x80406a34, buf) # JSIOCGBTNMAP
+    def show_map(self):
+        '''
+        list the buttons and axis found on this joystick
+        '''
+        print('%d axes found: %s' % (self.num_axes, ', '.join(self.axis_map)))
+        print('%d buttons found: %s' %
+              (self.num_buttons, ', '.join(self.button_map)))
 
-for btn in buf[:num_buttons]:
-    btn_name = button_names.get(btn, 'unknown(0x%03x)' % btn)
-    button_map.append(btn_name)
-    button_states[btn_name] = 0
+    def poll(self):
+        '''
+        query the state of the joystick, returns button which was pressed, if any,
+        and axis which was moved, if any. button_state will be None, 1, or 0 if no changes,
+        pressed, or released. axis_val will be a float from -1 to +1. button and axis will
+        be the string label determined by the axis map in init.
+        '''
+        button = None
+        button_state = None
+        axis = None
+        axis_val = None
 
-print('%d axes found: %s' % (num_axes, ', '.join(axis_map)))
-print('%d buttons found: %s' % (num_buttons, ', '.join(button_map)))
+        if self.jsdev is None:
+            return button, button_state, axis, axis_val
 
-# Main event loop
-while True:
-    evbuf = jsdev.read(8)
-    if evbuf:
-        time, value, type, number = struct.unpack('IhBB', evbuf)
+        # Main event loop
+        evbuf = self.jsdev.read(8)
 
-        if type & 0x80:
-             print("(initial)", end="")
+        if evbuf:
+            tval, value, typev, number = struct.unpack('IhBB', evbuf)
 
-        if type & 0x01:
-            button = button_map[number]
-            if button:
-                button_states[button] = value
-                if value:
-                    print("%s pressed" % (button))
-                else:
-                    print("%s released" % (button))
+            if typev & 0x80:
+                # ignore initialization event
+                return button, button_state, axis, axis_val
 
-        if type & 0x02:
-            axis = axis_map[number]
-            if axis:
-                fvalue = value / 32767.0
-                axis_states[axis] = fvalue
-                print("%s: %.3f" % (axis, fvalue))
+            if typev & 0x01:
+                button = self.button_map[number]
+                # print(tval, value, typev, number, button, 'pressed')
+                if button:
+                    self.button_states[button] = value
+                    button_state = value
+
+            if typev & 0x02:
+                axis = self.axis_map[number]
+                if axis:
+                    fvalue = value / 32767.0
+                    self.axis_states[axis] = fvalue
+                    axis_val = fvalue
+
+        return button, button_state, axis, axis_val
+
+
+class XboxOneJoystick(Joystick):
+    '''
+    An interface to a physical joystick 'Xbox Wireless Controller' controller.
+    This will generally show up on /dev/input/js0.
+    - Note that this code presumes the built-in linux driver for 'Xbox Wireless Controller'.
+      There is another user land driver called xboxdrv; this code has not been tested
+      with that driver.
+    - Note that this controller requires that the bluetooth disable_ertm parameter
+      be set to true; to do this:
+      - edit /etc/modprobe.d/xbox_bt.conf
+      - add the line: options bluetooth disable_ertm=1
+      - reboot to tha this take affect.
+      - after reboot you can vertify that disable_ertm is set to true entering this
+        command oin a terminal: cat /sys/module/bluetooth/parameters/disable_ertm
+      - the result should print 'Y'.  If not, make sure the above steps have been done corretly.
+
+    credit:
+    https://github.com/Ezward/donkeypart_ps3_controller/blob/master/donkeypart_ps3_controller/part.py
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super(XboxOneJoystick, self).__init__(*args, **kwargs)
+
+        self.axis_names = {
+            0x00: 'left_stick_horz',
+            0x01: 'left_stick_vert',
+            0x05: 'right_stick_vert',
+            0x02: 'right_stick_horz',
+            0x0a: 'left_trigger',
+            0x09: 'right_trigger',
+            0x10: 'dpad_horiz',
+            0x11: 'dpad_vert'
+        }
+
+        self.button_names = {
+            0x130: 'a_button',
+            0x131: 'b_button',
+            0x133: 'x_button',
+            0x134: 'y_button',
+            0x13b: 'options',
+            0x136: 'left_shoulder',
+            0x137: 'right_shoulder',
+        }
+
+
+if __name__ == "__main__":
+    import time
+
+    # Testing the XboxOneJoystickController
+    js = XboxOneJoystick('/dev/input/js0')
+    js.init()
+
+    while True:
+        button, button_state, axis, axis_val = js.poll()
+        if button is not None or axis is not None:
+            print(button, button_state, axis, axis_val)
+        time.sleep(0.01)
