a80f9671c6e967140d249f148e4f8ec0c9ee08a6
Maxime Ellerbach <maxime@ellerbach.net>
2021-08-28 15:28:49 +0200

    sync

--- joy_control/adjust_training2.py
+++ joy_control/adjust_training2.py
@@ -23,8 +23,9 @@ MAXTHROTTLE = 0.5
 th_steering = 0.05  # 5% threshold
 th_throttle = 0.06  # 6% threshold
 
-comPort = "/dev/ttyUSB0"
-ser = serial_command2.start_serial(comPort)
+serialport = "/dev/ttyUSB0"
+os.system('sudo chmod 0666 {}'.format(serialport))
+ser = serial_command2.start_serial(serialport)
 
 joy = controller.XboxOneJoystick()
 joy.init()
