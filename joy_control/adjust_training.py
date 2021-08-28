fdebe372b49025207a9032584531a1a9fcd74cbd
Maxime Ellerbach <maxime@ellerbach.net>
2021-08-28 15:56:05 +0200

    sync

--- joy_control/adjust_training.py
+++ joy_control/adjust_training.py
@@ -1,87 +1,88 @@
-'''
-joystick button usage:
-    'joy_steering': 'direction',
-    'joy_throttle': 'throttle',
-    'joy_brake': 'brake',
-    'joy_button_a': 'overide prediction and save img'
-    'joy_button_x': 'disable direction when joy_button_a is pressed'
-'''
-import cv2
-import os
-import time
 
-import xbox
-from custom_modules import serial_command, architectures, drive_utils
-from custom_modules.datasets import dataset_json
+import controller
+
+
+def deadzone(value, th, default=0):
+    return value if abs(value) > th else default
 
 
 Dataset = dataset_json.Dataset(["direction", "speed", "throttle", "time"])
-input_components = [1]
+input_components = []
 
-dos_save = os.getcwd()+os.path.normpath("/recorded/")
+dos_save = os.path.expanduser('~') + "/recorded/"
 if not os.path.isdir(dos_save):
     os.mkdir(dos_save)
 
-MAXTHROTTLE = 120
-th_direction = 0.05
-th_throttle = 0.1
+MAXTHROTTLE = 0.5
+th_steering = 0.05  # 5% threshold
+th_throttle = 0.06  # 6% threshold
 
-comPort = drive_utils.get_port_name()
-ser = serial_command.start_serial(comPort)
-joy = xbox.Joystick()
+serialport = "/dev/ttyUSB0"
+os.system('sudo chmod 0666 {}'.format(serialport))
+ser = serial_command2.start_serial(serialport)
+
+joy = controller.XboxOneJoystick()
+joy.init()
+assert joy.connected is True
+print("joy working")
+
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
+ret, img = cap.read()  # read the camera once to make sure it works
+assert ret is True
+print("cam working")
 
 basedir = os.path.dirname(os.path.abspath(__file__))
-model = architectures.load_model(
-    os.path.normpath(f'{basedir}/../test_model/models/rbrl_sim5_working.h5'))
-architectures.apply_predict_decorator(model)
+model = architectures.TFLite(
+    os.path.normpath(f'{basedir}/../test_model/models/auto_label6.tflite'), ['direction'])
 
-ser.ChangeMotorA(serial_command.Motor.MOTOR_FORWARD)
-while not joy.Back():
+print("Starting mainloop")
 
-    joy_steering, joy_throttle, joy_brake, joy_button_a, joy_button_x = drive_utils.get_controller_buttons(
-        joy)
-    current_speed = ser.GetCurrentSpeed()
-    _, img = cap.read()
+while not joy.button_states['back'] and joy.connected:
+    try:
+        joy_steering = joy.axis_states['x']
+        joy_throttle = joy.axis_states['rz']
+        joy_brake = joy.axis_states['z']
+        joy_button_a = joy.button_states['a']
+        joy_button_x = joy.button_states['x']
 
-    # annotation template with just what is needed for the prediction
-    annotation = {
-        'direction': 0,
-        'speed': current_speed,
-        'throttle': 0,
-        'time': time.time()
-    }
+        _, img = cap.read()
+        img = cv2.resize(img, (160, 120))
 
-    if joy_button_a:
-
-        steering = joy_steering if abs(joy_steering) > abs(
-            th_direction) and not joy_button_x else 0
-        throttle = joy_throttle - \
-            joy_brake if abs(joy_throttle) > abs(th_throttle) else 0
-
+        # annotation template with just what is needed for the prediction
         annotation = {
-            'direction': steering,
-            'speed': current_speed,
-            'throttle': throttle,
+            'direction': deadzone(joy_steering, th_steering),
+            'speed': 0,
+            'throttle': deadzone(joy_throttle - joy_brake, th_throttle),
             'time': time.time()
         }
-        Dataset.save_img_and_annotation(img, annotation)
-    else:
-        annotation_list = drive_utils.dict2list(annotation)
-        to_pred = Dataset.make_to_pred_annotations(
-            [img], [annotation_list], input_components)
 
-        output_dict, elapsed_time = model.predict(to_pred)
-        output_dict = output_dict[0]
-        for key in output_dict:
-            annotation[key] = output_dict[key]
+        if joy_button_a or joy_button_x:
 
-    pwm = int(MAXTHROTTLE * annotation['throttle'])
-    ser.ChangeDirection(drive_utils.direction2categorical(steering))
-    ser.ChangePWM(pwm)
+            if not joy_button_x:
+                Dataset.save_img_and_annotation(
+                    img,
+                    annotation=annotation,
+                    dos=dos_save)
 
+            ser.ChangeAll(annotation['direction'], annotation['throttle'])
 
-joy.close()
-print('terminated')
+        else:
+            to_pred = Dataset.make_to_pred_annotations(
+                [img], [annotation], input_components)
+
+            predicted, dt = model.predict(to_pred)
+            print(predicted)
+            ser.ChangeAll(predicted['direction'], MAXTHROTTLE * joy_throttle)
+
+    except Exception as e:
+        print(e)
+
+    except KeyboardInterrupt:
+        break
+
+ser.ChangeAll(0, 0)
+
+if not joy.connected:
+    print("Lost connection with joystick")
+else:
+    print('Terminated')
