7816889e6701018d1e7a29c874a289935a47dcf2
Maxime Ellerbach <maxime@ellerbach.net>
2021-08-28 17:01:04 +0200

    sync

--- random_programs/convert_model.py
+++ random_programs/convert_model.py
@@ -1,5 +1,5 @@
 from custom_modules import architectures
 
-model_path = 'test_model\\models\\auto_label4.h5'
-out = 'test_model\\models\\auto_label4.tflite'
-architectures.keras_model_to_tflite(model_path)
+model_path = 'test_model\\models\\auto_label6.h5'
+out = 'test_model\\models\\auto_label6.tflite'
+architectures.keras_model_to_tflite(model_path, out)
