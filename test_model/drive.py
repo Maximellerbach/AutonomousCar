548d9049a542423e73f02bcc379a2e2c28bbeee0
Maxime Ellerbach <maxime@ellerbach.net>
2021-09-11 01:22:58 +0200

    update

--- test_model/drive.py
+++ test_model/drive.py
@@ -10,7 +10,7 @@ def get_key_by_name(dict, name):
     for k in dict.keys():
         print(k, name, name in k)
         if name in k:
-            return k
+            return dict[k]
     return None
 
 
