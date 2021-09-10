c2645c157238aef7de573d0a3b5c537ba457bc63
Maxime Ellerbach <maxime@ellerbach.net>
2021-09-11 01:22:06 +0200

    update

--- test_model/drive.py
+++ test_model/drive.py
@@ -8,6 +8,7 @@ from custom_modules.datasets import dataset_json
 
 def get_key_by_name(dict, name):
     for k in dict.keys():
+        print(k, name, name in k)
         if name in k:
             return k
     return None
