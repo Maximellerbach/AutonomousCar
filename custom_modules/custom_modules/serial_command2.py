3b093f878cb91d0a0229acea1993c00ec72e509f
Maxime Ellerbach <maxime@ellerbach.net>
2021-11-11 15:53:21 +0100

    update

--- custom_modules/custom_modules/serial_command2.py
+++ custom_modules/custom_modules/serial_command2.py
@@ -87,6 +87,7 @@ class control:
                 # make sure that both end of lines are present
                 if out != "" and b'\r' in out and b'\n' in out:
                     res = int(out.decode())
+                    print(res)
 
                     if self.__pwm < 134 and self.__pwm > 120 and res > 25000 and res < 29000:  # no speed
                         self.__sensor_rpm = 0
