8b212f32080c87e51c68d80fe43f60222504ed98
Maxime Ellerbach <maxime@ellerbach.net>
2021-11-11 15:57:26 +0100

    update

--- custom_modules/custom_modules/serial_command2.py
+++ custom_modules/custom_modules/serial_command2.py
@@ -89,7 +89,9 @@ class control:
                     res = int(out.decode())
                     if self.pwm < 134 and self.pwm > 120 and res > 25000 and res < 29000:  # no speed
                         self.__sensor_rpm = 0
+                        print("no speed")
                     else:
+                        print("speed", self.pwm, res)
                         self.__sensor_rpm = (30000000 / res)
 
                 else:
