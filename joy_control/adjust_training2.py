7668c16c154c56ad3b3d0cb0ced1039f8156d8b7
Maxime Ellerbach <maxime@ellerbach.net>
2021-11-20 10:07:17 +0100

    update

--- joy_control/adjust_training2.py
+++ joy_control/adjust_training2.py
@@ -46,7 +46,7 @@ basedir = os.path.dirname(os.path.abspath(__file__))
 # model = architectures.safe_load_model(f"{basedir}/models/auto_label7.h5", compile=False)
 
 # Load TFLite model
-model = architectures.safe_load_model(f"{basedir}/../test_model/models/working_epita.tflite", ["direction"])
+model = architectures.safe_load_model(f"{basedir}/../test_model/models/working_renault.tflite", ["direction"])
 
 # checking if the controller is working properly
 joy_leftX = 0
@@ -98,7 +98,7 @@ while not joy.button_states["back"] and joy.connected and ret:
         print(prediction_dict, 1 / elapsed_time, 1 / dt)
 
     # apply direction and throttle
-    ser.ChangeAll(annotation["direction"]*0.8, MAXTHROTTLE * annotation["throttle"])
+    ser.ChangeAll(annotation["direction"], MAXTHROTTLE * annotation["throttle"])
 
 
 Memory.stop()
